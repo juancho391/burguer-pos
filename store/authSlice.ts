68d19bb91cc022e83553623b542238ec74d85f91
juancho391 <juanchischis2010@hotmail.com>
2025-11-04 12:40:56 -0500

    slice for global state with redux with methos for login and logout

---
+++ store/authSlice.ts
@@ -0,0 +1,27 @@
+"use client";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: typeof window !== "undefined" ? localStorage.getItem("token") : null,
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+      localStorage.setItem("token", action.payload.token);
+    },
+    logout: (state) => {
+      state.token = null;
+      state.user = null;
+      localStorage.removeItem("token");
+    },
+  },
+});
+
+export const { loginSuccess, logout } = authSlice.actions;
+export default authSlice.reducer;
