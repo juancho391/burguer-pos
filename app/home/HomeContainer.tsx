0eb4860087fb06500171a0f45287a2a6648f9ab5
juancho391 <juanchischis2010@hotmail.com>
2025-11-04 12:41:31 -0500

    basic home files

---
+++ app/home/HomeContainer.tsx
@@ -0,0 +1,7 @@
+export default function HomeContainer() {
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,5 @@
+import HomeContainer from "./HomeContainer";
+
+export default function Home() {
+  return <HomeContainer />;
+}
