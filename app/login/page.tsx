07d8c68e97946af59e697d25170eb3e88bdfafee
juancho391 <juanchischis2010@hotmail.com>
2025-11-04 12:37:27 -0500

    login page

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import LoginContainer from "./LoginContainer";
+
+export default function Login() {
+  return (
+    <main className="flex justify-center h-screen items-center">
+      <LoginContainer />
+    </main>
+  );
+}
