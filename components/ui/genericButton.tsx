33980a5e0b7e04ac35b2259871a40cbe16e157b6
juancho391 <juanchischis2010@hotmail.com>
2025-11-30 14:41:39 -0500

    i add a classname props for button

--- components/ui/genericButton.tsx
+++ components/ui/genericButton.tsx
@@ -1,11 +1,16 @@
+import { cn } from "@/lib/utils";
 export function GenericButton({
   children,
+  className,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
-      className="bg-orange-500 text-white px-4 py-2 rounded-xl hover:bg-orange-600 transition"
+      className={cn(
+        "bg-orange-500 text-white px-4 py-2 rounded-xl hover:bg-orange-600 transition",
+        className
+      )}
     >
       {children}
     </button>
