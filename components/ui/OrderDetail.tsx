6961f12648ebcb02dcdd96a97d47479b55cfb500
juancho391 <juanchischis2010@hotmail.com>
2025-12-09 19:41:06 -0500

    orderDetail component with productError

--- components/ui/OrderDetail.tsx
+++ components/ui/OrderDetail.tsx
@@ -1,16 +1,40 @@
 "use client";
 import { Order } from "@/types/order";
 import { GenericButton } from "./genericButton";
-import { useState } from "react";
+import { Key, useState } from "react";
 import { Checkbox } from "@heroui/checkbox";
 import OrderProductCard from "./OrderProductCard";
-export default function OrderDetail({ order }: { order: Order }) {
+import DropDownMenu from "./DropDownMenu";
+import { Product } from "@/types/product";
+
+export default function OrderDetail({
+  order,
+  products,
+  onSelect,
+  closeOrder,
+  deleteOrder,
+  productError,
+}: {
+  order: Order;
+  products: Product[];
+  onSelect: (key: Key) => void;
+  closeOrder: (order: Order, includeService: boolean) => void;
+  deleteOrder: (order: Order) => void;
+  productError: string;
+}) {
   const [includeService, setIncludeService] = useState(false);
+
+  const productItems = products.map((product) => {
+    return {
+      label: product.name,
+      key: product.id,
+    };
+  });
 
   return (
     <div className="w-full p-5">
-      <h3 className="text-2xl font-bold">Pedido #{order.id}</h3>
-      <label className="text-lg font-semibold">{order.customer}</label>
+      <h3 className="text-2xl font-bold ">Pedido #{order.id}</h3>
+      <label className="text-lg font-semibold ">{order.customer}</label>
       <div className="border border-black/20 w-full "></div>
       <div>
         {order.order_products.map((product) => (
@@ -28,6 +52,16 @@ export default function OrderDetail({ order }: { order: Order }) {
               )}
             </div>
           )}
+          {!order.is_paid && (
+            <div className="self-end rounded-full my-2 h-full">
+              <DropDownMenu items={productItems} onSelect={onSelect} />
+            </div>
+          )}
+          {productError && (
+            <div className="self-end rounded-full my-2 h-full">
+              <DropDownMenu items={productItems} onSelect={onSelect} />
+            </div>
+          )}
           <div className="flex justify-between w-full py-2">
             <label className="font-bold">Total</label>
             <label className="font-bold">${order.total_price}</label>
@@ -37,7 +71,7 @@ export default function OrderDetail({ order }: { order: Order }) {
       {order.is_paid ? (
         ""
       ) : (
-        <div className="flex flex-col">
+        <div className="flex flex-col py-2 w-full">
           <div className="flex justify-between">
             <label className="text-lg font-semibold">Incluir servicio</label>
             <Checkbox
@@ -47,7 +81,18 @@ export default function OrderDetail({ order }: { order: Order }) {
               onValueChange={setIncludeService}
             />
           </div>
-          <GenericButton className="mt-3">Pagar</GenericButton>
+          <GenericButton
+            onClick={() => closeOrder(order, includeService)}
+            className="mt-3"
+          >
+            Pagar
+          </GenericButton>
+          <GenericButton
+            onClick={() => deleteOrder(order)}
+            className="mt-3 bg-red-400 hover:bg-red-500"
+          >
+            Eliminar
+          </GenericButton>
         </div>
       )}
     </div>
