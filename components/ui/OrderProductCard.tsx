b3dcb9b04235e21b4a7e2e97e9f468b01bb8e350
juancho391 <juanchischis2010@hotmail.com>
2025-12-09 19:41:13 -0500

    order product card

--- components/ui/OrderProductCard.tsx
+++ components/ui/OrderProductCard.tsx
@@ -4,11 +4,11 @@ import { PlusCircleIcon, MinusCircleIcon } from "lucide-react";
 export default function OrderProductCard({ product }: { product: Product }) {
   return (
     <div className="flex  items-center w-full py-3 justify-between">
-      <div className="flex flex-col items-start">
-        <label className="font-semibold">{product.product_name}</label>
+      <div className="flex flex-col items-start w-1/5">
+        <label className="font-semibold">{product.name}</label>
         <label className="text-sm">${product.unit_price}</label>
       </div>
-      <div className="flex items-center justify-evenly bg-gray-400/30 p-1 rounded-full gap-2 ">
+      <div className="flex items-center justify-evenly bg-gray-400/30 p-1 rounded-full gap-2">
         <button className="cursor-pointer">
           <MinusCircleIcon color="gray" />
         </button>
